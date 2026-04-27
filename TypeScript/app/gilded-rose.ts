bfa63b78bc8d1a160c9304550df056a12b34df70
mbjelac <marko.bjelac@gmail.com>
2026-04-27 19:27:23 +0200

    r sulfurus for the win

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -26,6 +26,10 @@ export class GildedRose {
   }
 
   private update(item: Item) {
+    if (item.name == 'Sulfuras, Hand of Ragnaros') {
+      return;
+    }
+
     if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (item.quality > 0) {
         if (item.name != 'Sulfuras, Hand of Ragnaros') {
